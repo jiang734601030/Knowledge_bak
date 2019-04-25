c9158dc3e09dbd457eb6947ca168b49bc7080ff2
jiang734601030 <734601030@qq.com>
2019-04-25 10:03:25 +0800

    commit
    Signed-off-by: jiang734601030 <734601030@qq.com>

---
+++ JavaScript数据类型/index.js
@@ -0,0 +1,8 @@
+//4.15 JavaScript 数据类型
+
+
+// 值类型(基本类型)：字符串（String）、数字(Number)、布尔(Boolean)、对空（Null）、未定义（Undefined）、Symbol。
+//注：Symbol 是 ES6 引入了一种新的原始数据类型，表示独一无二的值。
+
+// 引用数据类型：对象(Object)、数组(Array)、函数(Function)。
+
---
+++ write方法/index.js
@@ -0,0 +1,6 @@
+//4.13 
+
+
+document.write("<h1>这是一个标题</h1>");
+document.write("<p>这是一个段落</p>");
+document.write("这是一个段落");//依次插入到document文档中
